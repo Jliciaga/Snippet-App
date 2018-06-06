ec4fabe29c9d4f7ea11e8e2b67cfe7727ee1366e
Jliciaga <Josiah.LiciagaSilva@gmail.com>
2018-06-05 23:02:49 -0700

    Added save functionality, updated styling

--- app.js
+++ app.js
@@ -17,29 +17,64 @@ $(document).ready(function() {
 	}
 
 	Storage.prototype.popArrayItem = function() {
-		var 
+		var arrayItem = {};
+		var existingArray = this.getArray(arrayName);
+		if(existingArray.length > 0) {
+			arrayItem = existingArray.pop();
+			this.setItem(arrayName, JSON.stringify(existingArray))
+		}
+		return arrayItem;
+	}
+
+	Storage.prototype.shiftArrayItem = function(arrayName) {
+		var arrayItem = {};
+		var existingArray = this.getArray(arrayName);
+		if(existingArray.length > 0) {
+			arrayItem = existingArray.pop();
+			this.setItem(arrayName, JSON.stringify(existingArray))
+		}
+		return arrayItem;
+	}
+
+	Storage.prototype.unshiftArrayItem = function(arrayName) {
+		var arrayItem = {};
+		var existingArray = this.getArray(arrayName);
+		existingArray.unshift(arrayItem);
+		this.setItem(arrayName, JSON.stringify(existingArray))
+	}
+
+	Storage.prototype.getLastItem = function(arrayName) {
+		var arrayItem = {};
+		var existingArray = this.getArray(arrayName);
+		if (existingArray.length > 0) {
+			arrayItem = exisitngArray[existingArray.length - 1];
+			this.setItem(arrayName, JSON.stringify(existingArray));
+		}
+		return arrayItem;
 	}
 
 	var data ={};
 	var $snippets = $('.snippets');
 
-	$('.savaData').on('click', function() {
-		let name = $('.codeName').val();
-		data[name] = $('.codeSnippet').val();
+	function save() {
+	var name = $('#name').val();
+	var code = $('#code').val();	
+	data[name] = code;
+		localStorage.pushArrayItem('data', data);
+	}
 
-		localStorage.pushArrayItem('dataArray', data)
+	$('#save').on('click', function() {
+	  save();
 	});
 
-	$('.callData').on('click', function() {
+	$('#delete').on('click', function() {
 		let retrieveLatest = localStorage.popArrayItem('dataArray');
 		for (var key in retrieveLatest[key]) {
-
-			$('.name').append(key);
-			$('#new-snippet').append(retrieveLatest[key])
 
 		}
 	});
 
+	
 
 
 })
